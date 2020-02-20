af9e0dabeeb8c2746a71bf2bd535f5bafc11bc11
frenby52 <aakytin@mail.ru>
2020-02-20 18:39:38 +0300

    adds tabs e2e

---
+++ src/components/tabs/tabs.e2e.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Tabs from "./Tabs";
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+
+const tabClickHandler = jest.fn();
+
+const tabs = shallow(<Tabs onTabClick={tabClickHandler} activeTab={1} />);
+
+const movieLink = tabs.find(`.movie-nav__link`).first();
+
+it(`Tabs component - click on tab correct`, () => {
+  movieLink.simulate(`click`, {preventDefault() {}});
+  expect(tabClickHandler).toHaveBeenCalledTimes(1);
+  expect(tabClickHandler).toBeCalledWith(0);
+});
+
